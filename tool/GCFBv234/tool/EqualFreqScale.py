4669e38e6c4ea2eedcf23a3225f165bc9575a978
nyaruko123 <sunhaozheng584@gmail.com>
2025-03-05 16:49:20 +0900

    Update EqualFreqScale.py

--- tool/GCFBv234/tool/EqualFreqScale.py
+++ tool/GCFBv234/tool/EqualFreqScale.py
@@ -9,7 +9,6 @@ Function [Frs, WFvals] = EqualFreqScale(NameScale, NumCh, RangeFreq)
         WFvals: Warped Frequency values
 """
 
-
 import numpy as np
 import librosa
 from .Freq2ERB import Freq2ERB
@@ -17,14 +16,15 @@ from .ERB2Freq import ERB2Freq
 
 def EqualFreqScale(NameScale, NumCh, RangeFreq):
 
-
     if len(RangeFreq) < 2 or RangeFreq[0] >= RangeFreq[1]:
         raise ValueError('RangeFreq(1) should be less than RangeFreq(2).')
 
+    # **确保 RangeFreq 是 numpy 数组**
+    RangeFreq = np.asarray(RangeFreq, dtype=np.float64)
+
     if NameScale.lower() == 'linear':
-        RangeWF = RangeFreq
-        dWF = (RangeWF[1] - RangeWF[0]) / (NumCh - 1)
-        WFvals = np.arange(RangeWF[0], RangeWF[1] + np.finfo(float).eps * 1000, dWF)
+        dWF = (RangeFreq[1] - RangeFreq[0]) / (NumCh - 1)
+        WFvals = np.arange(RangeFreq[0], RangeFreq[1] + np.finfo(float).eps * 1000, dWF)
         Frs = WFvals
 
     elif NameScale.lower() == 'mel':
@@ -34,9 +34,9 @@ def EqualFreqScale(NameScale, NumCh, RangeFreq):
         Frs = librosa.mel_to_hz(WFvals)
 
     elif NameScale.lower() == 'erb':
-        RangeWF = Freq2ERB(RangeFreq)
-        dWF = (RangeWF[1] - RangeWF[0]) / (NumCh - 1)
-        WFvals = np.arange(RangeWF[0], RangeWF[1] + np.finfo(float).eps * 1000, dWF)
+        ERBrate, _ = Freq2ERB(RangeFreq)  # 只取 ERBrate
+        dWF = (ERBrate[1] - ERBrate[0]) / (NumCh - 1)
+        WFvals = np.arange(ERBrate[0], ERBrate[1] + np.finfo(float).eps * 1000, dWF)
         Frs = ERB2Freq(WFvals)
 
     elif NameScale.lower() == 'log':
@@ -52,5 +52,3 @@ def EqualFreqScale(NameScale, NumCh, RangeFreq):
         raise ValueError('Specify NameScale correctly')
 
     return Frs, WFvals
-
-
