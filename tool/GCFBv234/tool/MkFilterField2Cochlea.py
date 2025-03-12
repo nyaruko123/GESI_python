447787182d2c5b940308fb6051272bc875fedc16
nyaruko123 <sunhaozheng584@gmail.com>
2025-03-12 14:11:38 +0900

    Update MkFilterField2Cochlea.py
    修复了使用remez函数时bands和desired长度不匹配的问题

--- tool/GCFBv234/tool/MkFilterField2Cochlea.py
+++ tool/GCFBv234/tool/MkFilterField2Cochlea.py
@@ -5,6 +5,7 @@ from scipy.signal import freqz
 from .OutMidCrct import OutMidCrct
 from scipy.signal import remez
 from .TaperWindow import TaperWindow
+
 def rceps(signal):
     spectrum = np.fft.fft(signal)
     log_spectrum = np.log(np.abs(spectrum))
@@ -14,45 +15,39 @@ def rceps(signal):
 def MkFilterField2Cochlea(StrCrct, fs, SwFwdBwd=1, SwPlot=0):
     global Param_Keep, fs_Keep, FIRCoefFwd_Keep, FIRCoefBwd_Keep
     global TypeField2EarDrum_Keep, TypeMidEar2Cochlea_Keep
-    #初始化
+    # 初始化
     TypeField2EarDrum_Keep = None
     TypeMidEar2Cochlea_Keep = None
     fs_Keep = None
     FIRCoefFwd_Keep = []
     FIRCoefBwd_Keep = []
     
-    # Initial setup
     if fs > 48000:
-        print(f'MkFilterField2Cochlea: Sampling rate of {fs} (Hz) (> 48000 (Hz)) is not recommended.')
-        print('<-- Transfer function is only defined below 16000 (Hz).')
+        print(f'MkFilterField2Cochlea: Sampling rate of {fs} Hz (> 48000 Hz) is not recommended.')
+        print('<-- Transfer function is only defined below 16000 Hz.')
     Param = {'fs': fs}
 
     if StrCrct.lower() in ['freefield', 'ff']:
         SwType = 1
         Param['TypeField2EarDrum'] = 'FreeField'
         Param['TypeMidEar2Cochlea'] = 'MiddleEar'
-
     elif StrCrct.lower() in ['diffusefield', 'df']:
         SwType = 2
         Param['TypeField2EarDrum'] = 'DiffuseField'
         Param['TypeMidEar2Cochlea'] = 'MiddleEar'
-
     elif StrCrct.lower() == 'itu':
         SwType = 3
         Param['TypeField2EarDrum'] = 'ITU'
         Param['TypeMidEar2Cochlea'] = 'MiddleEar'
-
     elif StrCrct.lower() in ['eardrum', 'ed']:
         SwType = 4
         Param['TypeField2EarDrum'] = 'NoField2EarDrum'
         Param['TypeMidEar2Cochlea'] = 'MiddleEar'
-
     elif StrCrct.lower() == 'elc':
         SwType = 10
         Param['TypeField2CochleadB'] = 'ELC'
         Param['TypeField2EarDrum'] = 'NoUse_ELC'
         Param['TypeMidEar2Cochlea'] = 'NoUse_ELC'
-
     else:
         raise ValueError('Specify: FreeField (FF) / DiffuseField (DF) / ITU / EarDrum (ED) / ELC')
 
@@ -65,34 +60,16 @@ def MkFilterField2Cochlea(StrCrct, fs, SwFwdBwd=1, SwPlot=0):
 
     Param['NameFilter'] = f'[{StrCrct}] {Param["NameFilter"]}'
 
-    # No Calculation. Restoring from the kept data
-    if (TypeField2EarDrum_Keep == Param['TypeField2EarDrum'] and
-        TypeMidEar2Cochlea_Keep == Param['TypeMidEar2Cochlea'] and
-        fs_Keep == fs and SwPlot == 0):
-        if SwFwdBwd == 1 and len(FIRCoefFwd_Keep) > 20:
-            FIRCoef = FIRCoefFwd_Keep
-            print(f'*** MkFilterField2Cochlea: Restoring {Param["NameFilter"]} ***')
-            Param = Param_Keep
-            return FIRCoef, Param
-
-        elif SwFwdBwd == -1 and len(FIRCoefBwd_Keep) > 20:
-            FIRCoef = FIRCoefBwd_Keep
-            print(f'*** MkFilterField2Cochlea: Restoring {Param["NameFilter"]} ***')
-            Param = Param_Keep
-            return FIRCoef, Param
-
-    # Generating filter at the first time
     print(f'*** MkFilterField2Cochlea: Generating {Param["NameFilter"]} ***')
 
     if SwType <= 4:
-        Param['TypeMidEar2Cochlea'] = 'MiddleEar'
-        TransFunc,ParamOut  = TransFuncField2Cochlea(Param)  # Undefined function
+        TransFunc, ParamOut = TransFuncField2Cochlea(Param)
         FrspCrct = 10 ** (TransFunc['Field2CochleadB'] / 20)
         freq = TransFunc['freq']
         Param['TypeField2CochleadB'] = TransFunc['TypeField2CochleadB']
     elif SwType == 10:
         Nrslt = 2048
-        crctPwr, freq = OutMidCrct(StrCrct, Nrslt, fs, 0)  # Undefined function
+        crctPwr, freq = OutMidCrct(StrCrct, Nrslt, fs, 0)
         FrspCrct = np.sqrt(crctPwr)
 
     if SwFwdBwd == -1:
@@ -100,21 +77,52 @@ def MkFilterField2Cochlea(StrCrct, fs, SwFwdBwd=1, SwPlot=0):
 
     try:
         LenCoef = 200
-        NCoef = int(LenCoef / 16000 * fs / 2) * 2
-        FIRCoef = remez(NCoef, freq / fs * 2, FrspCrct)
-    except:
-        print('-- For octave compatibility --')
-        LenCoef = 50
-        NCoef = int(LenCoef / 16000 * fs / 2) * 2
-        FIRCoef = remez(NCoef, freq / fs * 2, FrspCrct)
+        NCoef = max(int(LenCoef / 16000 * fs / 2) * 2, 2)  # 保证 NCoef 是偶数，且至少为 2
 
-    Win = TaperWindow(len(FIRCoef), 'han', LenCoef / 10)  # Undefined function
+        freq_norm = np.array(freq) / (fs / 2)  # 归一化频率
+        freq_norm = np.clip(freq_norm, 0, 1)  # 确保频率在 0~1 之间
+
+        #===== 修改关键部分：修正频带与期望响应的维度关系 =====#
+        bands = []
+        desired = []
+        # 构造连续的频带区间
+        for i in range(len(freq_norm)-1):
+            bands.extend([freq_norm[i], freq_norm[i+1]])  # 每个频带两个边界
+            desired.append(FrspCrct[i])  # 每个频带对应一个期望值
+        
+        weights = np.ones(len(desired))  # 权重数组长度与desired一致
+        
+        # 添加维度验证
+        if len(bands) != 2 * len(desired):
+            raise ValueError(f"频带维度异常 bands({len(bands)}) != 2*desired({len(desired)})")
+
+        FIRCoef = remez(NCoef, bands, desired, weight=weights, fs=fs)
+
+    except Exception as e:
+        print('-- For octave compatibility --')
+        print(f'Error: {e}')
+        # 异常处理时重新生成有效参数
+        LenCoef = 50
+        NCoef = max(int(LenCoef / 16000 * fs / 2) * 2, 2)
+        # 构造降采样后的频带参数
+        downsample_ratio = max(1, len(freq_norm)//100)  # 防止频点过多
+        freq_norm_simple = freq_norm[::downsample_ratio]
+        FrspCrct_simple = FrspCrct[::downsample_ratio]
+        
+        bands = []
+        desired = []
+        for i in range(len(freq_norm_simple)-1):
+            bands.extend([freq_norm_simple[i], freq_norm_simple[i+1]])
+            desired.append(FrspCrct_simple[i])
+        
+        FIRCoef = remez(NCoef, bands, desired, weight=weights[:len(desired)], fs=fs)
+
+    Win = TaperWindow(len(FIRCoef), 'han', LenCoef / 10)
     FIRCoef = Win * FIRCoef
 
     _, x_mp = rceps(FIRCoef)
     FIRCoef = x_mp[:len(x_mp) // 2]
 
-    # Keep records for fast processing
     if SwFwdBwd == 1:
         FIRCoefFwd_Keep = FIRCoef
     elif SwFwdBwd == -1:
@@ -125,26 +133,15 @@ def MkFilterField2Cochlea(StrCrct, fs, SwFwdBwd=1, SwPlot=0):
     TypeField2EarDrum_Keep = Param['TypeField2EarDrum']
     TypeMidEar2Cochlea_Keep = Param['TypeMidEar2Cochlea']
 
-    # Plot
     if SwPlot == 1:
         Nrsl = len(FrspCrct)
         frsp, freq2 = freqz(FIRCoef, 1, Nrsl, fs)
         import matplotlib.pyplot as plt
-        plt.subplot(2, 1, 1)
-        plt.plot(FIRCoef)
-        plt.xlabel('Sample')
-        plt.ylabel('Amplitude')
-        plt.title(f'Type: {Param["TypeField2EarDrum"]}')
-
-        plt.subplot(2, 1, 2)
-        plt.plot(freq2, np.abs(frsp), freq, FrspCrct, '--')
+        plt.plot(freq2, np.abs(frsp), label="Filter Response")
+        plt.plot(freq, FrspCrct, '--', label="Target Response")
+        plt.legend()
         plt.xlabel('Frequency (Hz)')
-        plt.ylabel('Amplitude (linear term)')
-        ELCError = np.mean((np.abs(frsp) - FrspCrct) ** 2) / np.mean(FrspCrct ** 2)
-        ELCErrordB = 10 * np.log10(ELCError)
-
-        print(f'Fitting Error: {ELCErrordB} (dB)')
-        if ELCErrordB > -30:
-            print(f'Warning: Error in ELC correction = {ELCErrordB} dB > -30 dB')
+        plt.ylabel('Amplitude')
+        plt.show()
 
     return FIRCoef, Param
